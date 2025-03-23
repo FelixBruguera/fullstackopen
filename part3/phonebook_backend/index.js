034d6e2631c40752a682636214cd48b61b8b3d7d
Felix Bruguera <felixbruguera0@gmail.com>
2025-03-23 07:49:16 -0400

    Finish part 3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,53 +1,53 @@
 require('dotenv').config()
-const express = require("express")
-const Person = require("./models/person")
+const express = require('express')
+const Person = require('./models/person')
 const app = express()
 var morgan = require('morgan')
 app.use(express.static('dist'))
 app.use(express.json())
 
-morgan.token("body", (req) => JSON.stringify(req.body))
-app.use(morgan("tiny"))
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan('tiny'))
 app.use(morgan(':body'))
 
 const PORT = process.env.PORT
 app.listen(PORT)
 
-app.get("/api/persons", (request, response) => {
-    Person.find({})
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
     .then(data => response.send(data))
     .catch(error => next(error))
 })
 
-app.get("/info", (request, response) => {
-    Person.find({})
-    .then(data => response.send(`Phonebook has info of ${data.length} people` + "<br> <br>" + new Date()))
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(data => response.send(`Phonebook has info of ${data.length} people` + '<br> <br>' + new Date()))
     .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response, next) => {
-    const id = request.params.id
-    Person.findById(id)
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id)
     .then(person => person ? response.send(person) : response.status(404).end())
     .catch(error => next(error))
 })
 
-app.put("/api/persons/:id", (request, response, next) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  Person.findByIdAndUpdate(id, {name: request.body.name, number: request.body.number}, {new: true, runValidators: true})
-  .then(person => response.send(person))
-  .catch(error => next(error))
+  Person.findByIdAndUpdate(id, { name: request.body.name, number: request.body.number }, { new: true, runValidators: true })
+    .then(person => response.send(person))
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id)
-    .then(data => response.status(204).end())
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => response.status(204).end())
 })
 
-app.post("/api/persons", (request, response, next) => {
-    const newPerson = new Person({"name": request.body.name, "number": request.body.number})
-    newPerson.save()
+app.post('/api/persons', (request, response, next) => {
+  const newPerson = new Person({ name: request.body.name, number: request.body.number })
+  newPerson.save()
     .then(data => response.status(200).send(data))
     .catch(error => next(error))
 })
@@ -55,10 +55,10 @@ app.post("/api/persons", (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
   console.log(error.name)
 
-  if (error.name === "CastError") {
-    return response.status(400).send("Malformed ID")
+  if (error.name === 'CastError') {
+    return response.status(400).send('Malformed ID')
   }
-  if (error.name === "ValidationError") {
+  if (error.name === 'ValidationError') {
     return response.status(400).send(error.message)
   }
 
