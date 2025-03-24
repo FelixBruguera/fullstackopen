9173bb722901ceccbd05b0d397d0944ad4a3223c
Felix Bruguera <felixbruguera0@gmail.com>
2025-03-24 14:25:00 -0400

    Add part4 and finish steps 1 and 2 of blog list

---
+++ part4/bloglist/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
+const mongoUrl = config.DB_URL
+
+mongoose.connect(mongoUrl)
+.then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
